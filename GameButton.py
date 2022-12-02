f28585e8ae7aadf04efc1eb15f514ca989e86d14
thutha048 <thuthao4888@gmail.com>
2022-12-02 22:47:45 +0700

    othello

--- GameButton.py
+++ GameButton.py
@@ -12,30 +12,4 @@ class GameButton(Button):
     # Currently not used            
     def get_brick(self):
         return self.brick
-
-    #def animate_flip():
-    #def change_icon():
-
-    def draw_cell(self, row: int, col: int) -> None:
-        ''' Draws the specified cell '''
-        self.create_oval(col * self.get_cell_width(),
-                                row * self.get_cell_height(),
-                                (col + 1) * self.get_cell_width(),
-                                (row + 1) * self.get_cell_height()
-                               )                         
-
-    def get_cell_width(self) -> float:
-        ''' Returns a game cell's width '''
-        return 600 / self.get_columns()
-
-    def get_cell_height(self) -> float:
-        ''' Returns a game cell's height '''
-        return 750 / self.get_rows()
-
-    def get_board_width(self) -> float:
-        ''' Returns the board canvas's width '''
-        return float(self._board.winfo_width())
-
-    def get_board_height(self) -> float:
-        ''' Returns the board canvas's height '''
-        return float(self._board.winfo_height())
+        
